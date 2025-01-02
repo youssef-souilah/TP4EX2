155dfb0664503339b6a44cc93081f29d4578cf27
Youssef Souilah <souilah.youssef.0@gmail.com>
2025-01-02 21:35:24 +0100

    modifier

--- src/Transport/Vehicule.java
+++ src/Transport/Vehicule.java
@@ -5,6 +5,29 @@ public class Vehicule {
 	private String marque;
 	private int dateAchat;
 	private double prixAchat ,prixCourant;
+
+	public Vehicule(String marque, int dateAchat, double prixAchat) {
+		this.marque = marque;
+		this.dateAchat = dateAchat;
+		this.prixAchat = prixAchat;
+	}
+	
+	public void affiche() {
+		System.out.println("|| Marque : "+this.marque+" Date Achat : "+this.dateAchat+" Prix Achat : "+this.prixAchat+" Prix Courant : "+this.prixCourant+"||");
+	}
+	public void calculePrix(int anneActuelle) {
+		if(anneActuelle>this.dateAchat) {
+			int dif= anneActuelle-this.dateAchat;
+			double prix=this.prixAchat;
+			for(int i=0;i<dif;i++) {
+				prix=prix-(this.prixAchat*0.02);
+			}
+			this.prixCourant=prix;
+		}
+		else{
+			this.prixCourant=this.prixAchat;
+		}
+	}
 	
 	public String getMarque() {
 		return marque;
@@ -36,28 +59,5 @@ public class Vehicule {
 
 	public void setPrixCourant(double prixCourant) {
 		this.prixCourant = prixCourant;
-	}
-
-	public Vehicule(String marque, int dateAchat, double prixAchat) {
-		this.marque = marque;
-		this.dateAchat = dateAchat;
-		this.prixAchat = prixAchat;
-	}
-	
-	public void affiche() {
-		System.out.println("|| Marque : "+this.marque+" Date Achat : "+this.dateAchat+" Prix Achat : "+this.prixAchat+" Prix Courant : "+this.prixCourant+"||");
-	}
-	public void calculePrix(int anneActuelle) {
-		if(anneActuelle>this.dateAchat) {
-			int dif= anneActuelle-this.dateAchat;
-			double prix=this.prixAchat;
-			for(int i=0;i<dif;i++) {
-				prix=prix-(this.prixAchat*0.02);
-			}
-			this.prixCourant=prix;
-		}
-		else{
-			this.prixCourant=this.prixAchat;
-		}
 	}
 }
